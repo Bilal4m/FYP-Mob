2d49f07bf62803d7d4e79d00888bff9695c6f9fb
bilal <bilal_4m@live.com>
2021-11-10 00:24:02 +0500

    FYP-Project

---
+++ app/src/main/java/com/example/fyp_project/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.fyp_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.signup_btn1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
